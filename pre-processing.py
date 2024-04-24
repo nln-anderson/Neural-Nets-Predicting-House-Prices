dcf8d0fe33ee6a4382380cf9ebc6248703248569
Nolan Anderson <nolananderson@sandiego.edu>
2024-04-24 14:09:44 -0700

    added VIF table

--- pre-processing.py
+++ pre-processing.py
@@ -24,3 +24,6 @@ vif_data = pd.DataFrame()
 vif_data["feature"] = df.columns
 vif_data['VIF'] = [variance_inflation_factor(df.values, i) for i in range(len(df.columns))]
 print(vif_data)
+# vif_data.to_csv("vif.csv")
+import dataframe_image as dfi
+dfi.export(vif_data,"vif.png")
